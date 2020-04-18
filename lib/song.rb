53d03927694d9f2ac4a63188cad6f8d335e5ba8d
Taulant Ibraj <tibraj94@gmail.com>
2020-04-18 17:03:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,8 @@ class Song
   end
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
     artist.add_song(self)
   end
 
